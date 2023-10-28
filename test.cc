534b42cd09b5a0a6a16524172e7bf47afc503a4f
Trozure <2819058133@qq.com>
2023-10-28 19:53:29 +0800

    \'test.cc\' modified

--- test.cc
+++ test.cc
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <iostream>
 #include <sys/_types/_size_t.h>
 #include <unordered_map>
@@ -6,32 +7,8 @@ using std::cin;
 using std::cout;
 
 int main(int argc, char const *argv[]) {
-  std::ios::sync_with_stdio(false);
-
-  size_t n;
-  cin >> n;
-  for (size_t i = 0; i < n; ++i) {
-    std::unordered_map<char, int> map;
-    for (size_t j = 0; j < 4; j++) {
-      char temp;
-      cin >> temp;
-      if (map.find(temp) == map.end()) {
-        map[temp] = 1;
-        continue;
-      }
-      map[temp]++;
-    }
-
-    auto isMatch = [&]() -> bool {
-      for (const auto &pair : map) {
-        if (pair.second == 3) {
-          return true;
-        }
-      }
-      return false;
-    };
-
-    cout << (isMatch() ? "Yes" : "No") << std::endl;
-  }
+  system("git add --all");
+  system("git commit -m \"\\\'test.cc\\\' modified\"");
+  system("git push -u origin master");
   return 0;
 }
